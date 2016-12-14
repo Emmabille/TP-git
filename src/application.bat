50be2d55092a7ae208ab5d83369233a1c5113f66
Emmanuelle Billard <e.billard@gmail.com>
2016-12-14 16:34:17 +0100

    add partner name

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,4 @@
 ### application.bat - 2016 - Billard ###
 echo "Copyright Billard - 2016"
 echo "Build : 10:00"
-echo "version1.1"
+echo "version1.2"
---
+++ test/test_app.bat
@@ -0,0 +1,1 @@
+### test_app.bat - 2016 - Billard ###
